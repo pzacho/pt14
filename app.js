acaca7b55eb32e01895e2894245d01f7c101a3ed
Per Zacho <per.zacho@grandvision.com>
2018-04-03 22:51:10 +0200

    Updates

--- app.js
+++ app.js
@@ -2,4 +2,8 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+
+var UserController = require('./user/UserController');
+app.use('/users', UserController);
+
 module.exports = app;
--- db.js
+++ db.js
@@ -1,3 +1,3 @@
 // db.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://192.168.14.39:27017');
+mongoose.connect('mongodb://192.168.14.39:27017/pt14');
